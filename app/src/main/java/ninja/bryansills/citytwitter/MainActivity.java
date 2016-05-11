0b457e75186c6a0860e13f857f0387ac6a84d1a1
Bryan Sills <bryan@bryansills.com>
2016-05-10 22:33:23 -0500

    time sucks... but moshi now works...

--- app/src/main/java/ninja/bryansills/citytwitter/MainActivity.java
+++ app/src/main/java/ninja/bryansills/citytwitter/MainActivity.java
@@ -2,8 +2,15 @@ package ninja.bryansills.citytwitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Rfc3339DateJsonAdapter;
+
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import retrofit2.Call;
@@ -19,9 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Moshi moshi = new Moshi.Builder()
+                .add(Date.class, new Rfc3339DateJsonAdapter())
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://damp-dawn-82482.herokuapp.com")
-                .addConverterFactory(MoshiConverterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
                 .build();
 
         CityService cityService = retrofit.create(CityService.class);
@@ -29,11 +40,15 @@ public class MainActivity extends AppCompatActivity {
         cityService.tweets().enqueue(new Callback<List<Tweet>>() {
             @Override
             public void onResponse(Call<List<Tweet>> call, Response<List<Tweet>> response) {
+                for (Tweet tweet : response.body()) {
+                    Log.d("BLARG", tweet.toString());
+                }
                 Toast.makeText(MainActivity.this, response.body().toString(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<List<Tweet>> call, Throwable t) {
+                Log.d("BLARG", t.getLocalizedMessage());
                 Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
