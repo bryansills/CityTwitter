8192fc3d99088dbb6b0352fa97ec6daf189eec81
Bryan Sills <bryan.sills@isobar.com>
2016-05-18 17:27:46 -0500

    add JobScheduler support

--- app/src/main/java/ninja/bryansills/citytwitter/MainActivity.java
+++ app/src/main/java/ninja/bryansills/citytwitter/MainActivity.java
@@ -1,30 +1,29 @@
 package ninja.bryansills.citytwitter;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.squareup.moshi.Moshi;
-import com.squareup.moshi.Rfc3339DateJsonAdapter;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int MSG_SERVICE_OBJ = 2;
+
     private LocalDataManager localDataManager;
+    ComponentName serviceComponent;
+    private GetTweetsService getTweetsService;
 
     private RecyclerView recyclerView;
     private TweetAdapter tweetAdapter;
@@ -46,48 +45,41 @@ public class MainActivity extends AppCompatActivity {
 
         if (tweets != null) {
             Log.d("BLARG", "Loading tweets from local storage");
-            tweetAdapter.setTweetList(tweets);
+            loadTweets(tweets);
         } else {
             Log.d("BLARG", "Loading tweets from the network");
-            makeNetworkCall();
+            serviceComponent = new ComponentName(this, GetTweetsService.class);
+
+            Intent startServiceIntent = new Intent(this, GetTweetsService.class);
+            startServiceIntent.putExtra("messenger", new Messenger(mHandler));
+            startService(startServiceIntent);
         }
     }
 
-    private void makeNetworkCall() {
-        Moshi moshi = new Moshi.Builder()
-                .add(Date.class, new Rfc3339DateJsonAdapter())
-                .build();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://damp-dawn-82482.herokuapp.com")
-                .client(getOkHttpClient())
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .build();
-
-        CityService cityService = retrofit.create(CityService.class);
-
-        cityService.tweets().enqueue(new Callback<List<Tweet>>() {
-            @Override
-            public void onResponse(Call<List<Tweet>> call, Response<List<Tweet>> response) {
-                for (Tweet tweet : response.body()) {
-                    Log.d("BLARG", tweet.toString());
-                }
-                localDataManager.set(LocalDataManager.TWEETS, response.body());
-                tweetAdapter.setTweetList(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<List<Tweet>> call, Throwable t) {
-                Log.d("BLARG", t.getLocalizedMessage());
-                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void loadTweets(List<Tweet> tweets) {
+        tweetAdapter.setTweetList(tweets);
     }
 
-    private OkHttpClient getOkHttpClient() {
-        return new OkHttpClient.Builder()
-                .readTimeout(60, TimeUnit.SECONDS)
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .build();
-    }
+    Handler mHandler = new Handler(/* default looper */) {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SERVICE_OBJ:
+                    getTweetsService = (GetTweetsService) msg.obj;
+                    getTweetsService.setUiCallback(MainActivity.this);
+
+                    JobScheduler tm =
+                            (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+
+                    JobInfo.Builder builder = new JobInfo.Builder(1, serviceComponent)
+                            .setMinimumLatency(15 * 1000)
+                            .setOverrideDeadline(30 * 1000)
+                            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                            .setRequiresCharging(false)
+                            .setRequiresDeviceIdle(false);
+
+                    tm.schedule(builder.build());
+            }
+        }
+    };
 }
